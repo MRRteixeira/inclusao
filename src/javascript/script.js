57dfdbc4c298762ea4c327d7f7e9d281c8b42a43
marcosteixeira027 <140124464+marcosteixeira027@users.noreply.github.com>
2024-05-01 15:48:37 -0300

    adicao de arquivos

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,14 +4,25 @@ document.getElementById('open_btn').addEventListener('click', function () {
 
 function search() {
     var searchTerm = document.getElementById("searchInput").value;
-    alert("Você pesquisou por: " + searchTerm);
+    console.log("Você pesquisou por: " + searchTerm);
   }
 
   //barra de pesquisa//
 
   const data = [
     {
+     title: "alimentos",
+     description: "Alimento é aquilo que os seres vivos comem e bebem para a sua subsistência. "
+    },
+    
+    {
+    title: "alfabeto",
+    description:"Alfabeto ou Abecedário é uma forma de escrita de signos e significados classificada como segmental."
+    },
 
+    {
+     title: "numeros", 
+     description: "Números são conceitos matemáticos que representam medida, ordem ou quantidade. "
     },
   ];
 
